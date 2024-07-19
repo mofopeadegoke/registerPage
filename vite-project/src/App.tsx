c9ec79e0c76ce2ec6a25ff0c2d2720a1e3cb2b21
mofopeadegoke <mofopeadegoke@gmail.com>
2024-07-19 11:06:23 +0300

    page almost done

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import RegBox from "./components/register";
+import sideImg from "./images/image.svg";
+
+export default function RegPage() {
+  return (
+    <>
+      <main>
+        <RegBox />
+        <img
+          src={sideImg}
+          alt="A person floating mid air and smiling with a flower vase below them"
+          className="leftImg"
+        />
+      </main>
+      <footer></footer>
+    </>
+  );
+}
---
+++ vite-project/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RegPage from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RegPage />
+  </React.StrictMode>
+);
